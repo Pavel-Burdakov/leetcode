5b1ac9c7f903cbcf3362d3f0fb209b36629fca14
Pavel-Burdakov <127017286+Pavel-Burdakov@users.noreply.github.com>
2025-03-15 10:25:06 +0300

    Time: 1 ms (65.39%) | Memory: 41.6 MB (51.16%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,13 +1,9 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        // сортируем массив по возрастанию, чтобы самые короткие строки были вначале    
-        Arrays.sort(strs);
-        // в s1 - первая самая короткая строка
-        String s1 = strs[0];
-        // в s2 последняя самая длиная
-        String s2 = strs[strs.length-1];
-        // сюда будем записывать количество первых одинаковых элементов
-        int idx = 0;
+    public String longestCommonPrefix(String[] strs) {        
+        Arrays.sort(strs); // сортируем массив по возрастанию, чтобы самые короткие строки были вначале        
+        String s1 = strs[0]; // в s1 - первая самая короткая строка   
+        String s2 = strs[strs.length-1]; // в s2 последняя самая длиная  
+        int idx = 0; // сюда будем записывать количество первых одинаковых элементов
         while(idx < s1.length()){
             if (s1.charAt(idx) == s2.charAt(idx)){
               idx++;
