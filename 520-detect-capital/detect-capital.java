4f688eb80b0251ba11acfc1e626a907c69ea7347
Pavel-Burdakov <127017286+Pavel-Burdakov@users.noreply.github.com>
2025-03-17 20:54:34 +0300

    Time: 7 ms (6.23%) | Memory: 42.7 MB (5.16%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean detectCapitalUse(String word) {
         String up = word.toUpperCase();
         String low = word.toLowerCase();
-        String first = word.substring(0,1).toUpperCase();
-        String firstUp = first + word.substring(1, word.length()).toLowerCase();
+        String firstUp = word.substring(0,1).toUpperCase() + word.substring(1, word.length()).toLowerCase();
         System.out.println (firstUp);
         if (up.equals(word)||low.equals(word)||firstUp.equals(word)){
             return true;
